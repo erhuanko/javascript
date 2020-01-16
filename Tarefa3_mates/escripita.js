05d8f6c14737ca55a03cedf717be490de5601771
Juan <56600944+erhuanko@users.noreply.github.com>
2020-01-16 09:06:30 +0100

    Add files via upload

---
+++ Tarefa3_mates/escripita.js
@@ -0,0 +1,20 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+
+var muito = 9;
+var wenas = 25.222;
+var pipo = "Lil bokeron versos perversos is a really good one, he is a anbilibabol artist";
+var quepasa = 2.2134;
+var raisone = Math.sqrt(muito);
+var raistwo = Math.sqrt(wenas);
+var raisthree = Math.sqrt(pipo);
+var mates = Math.pow(quepasa,2);
+alert(raisone);
+alert(raistwo);
+alert(raisthree);
+alert(mates);
+alert(pipo);
+alert(Math.PI);
