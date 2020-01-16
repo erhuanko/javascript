0007301a8b5a36d571b0b8932253d162e24d61f4
Juan <56600944+erhuanko@users.noreply.github.com>
2020-01-16 09:34:56 +0100

    Add files via upload

---
+++ Tarefa5_ducias2ovos/script.js
@@ -0,0 +1,19 @@
+var ducias;
+ducias = prompt("Cantas ducias levan a tua tortilla española    ");
+
+var numero;
+numero=parseInt(ducias);
+alert(numero);
+var cuenta;
+cuenta=numero*12
+alert("a tua tortilla ten " + cuenta + " buebos")
+
+//var resposta;
+//resposta = prompt("Cuantos euros quieres convertir?     ");
+
+//var numero;
+//numero=parseInt(resposta);
+//alert(numero);
+//var cuenta;
+//cuenta=numero*122.51
+//alert(numero +" euro/s son " + cuenta + " yenes")
